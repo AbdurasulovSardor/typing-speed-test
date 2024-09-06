07cf1a6653ee1c71524bab39376ef5b354e3f2c8
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-09-06 23:59:31 +0500

    added js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const typingText = document.querySelector(".typing-text p")
 const inpField = document.querySelector(".wrapper .input-field")
+const timeTag = document.querySelector(".time span b")
 const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector(".wpm span")
+const cpmTag = document.querySelector(".mistake span")
 
-let charIndex = mistakes = 0
+let charIndex = mistakes = isTyping = 0
+let maxTime = 60
+let timeLeft = maxTime
+let timer
 
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length)
@@ -18,6 +24,10 @@ function randomParagraph() {
 function initTyping() {
   const character = typingText.querySelectorAll("span")
   let typedChar = inpField.value.split("")[charIndex]
+  if (!isTyping) {
+    timer = setInterval(initTimer, 1000)
+    isTyping = true
+  }
   if (typedChar == null) {
     charIndex--
     if (character[charIndex].classList.contains("incorrect")) {
@@ -36,7 +46,19 @@ function initTyping() {
   character.forEach(span => span.classList.remove("active"))
   character[charIndex].classList.add("active")
 
+  let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60)
   mistakeTag.innerText = mistakes
+  wpmTag.innerText = wpm
+  cpmTag.innerText = charIndex - mistakes
+}
+
+function initTimer() {
+  if (timeLeft > 0) {
+    timeLeft--
+    timeTag.innerText = timeLeft
+  } else {
+    clearInterval(timer)
+  }
 }
 
 randomParagraph()
