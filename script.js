499a07bddbb7b59873882cb02b13a60cd3a9e90a
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-09-09 21:58:18 +0500

    added js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const inpField = document.querySelector(".wrapper .input-field")
 const timeTag = document.querySelector(".time span b")
 const mistakeTag = document.querySelector(".mistake span")
 const wpmTag = document.querySelector(".wpm span")
-const cpmTag = document.querySelector(".mistake span")
+const cpmTag = document.querySelector(".cpm span")
+const tryAgainBtn = document.querySelector("button")
 
 let charIndex = mistakes = isTyping = 0
 let maxTime = 60
@@ -12,44 +13,53 @@ let timer
 
 function randomParagraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length)
+  typingText.innerHTML = ""
   paragraphs[randIndex].split("").forEach(span => {
     let spanTag = `<span>${span}</span>`
     typingText.innerHTML += spanTag
   })
+  typingText.querySelectorAll("span")[0].classList.add("active")
 
   document.addEventListener("keydown", () => inpField.focus())
   typingText.addEventListener("click", () => inpField.focus())
 }
 
 function initTyping() {
-  const character = typingText.querySelectorAll("span")
+  const characters = typingText.querySelectorAll("span")
   let typedChar = inpField.value.split("")[charIndex]
-  if (!isTyping) {
-    timer = setInterval(initTimer, 1000)
-    isTyping = true
-  }
-  if (typedChar == null) {
-    charIndex--
-    if (character[charIndex].classList.contains("incorrect")) {
-      mistakes--
+
+  if (charIndex < characters.length - 1 && timeLeft > 0) {
+    if (!isTyping) {
+      timer = setInterval(initTimer, 1000)
+      isTyping = true
     }
-    character[charIndex].classList.remove("correct", "incorrect")
-  } else {
-    if (character[charIndex].innerHTML === typedChar) {
-      character[charIndex].classList.add("correct")
+    if (typedChar == null) {
+      charIndex--
+      if (characters[charIndex].classList.contains("incorrect")) {
+        mistakes--
+      }
+      characters[charIndex].classList.remove("correct", "incorrect")
     } else {
-      character[charIndex].classList.add("incorrect")
+      if (characters[charIndex].innerHTML === typedChar) {
+        characters[charIndex].classList.add("correct")
+      } else {
+        characters[charIndex].classList.add("incorrect")
+      }
+      charIndex++
     }
-    charIndex++
+
+    characters.forEach(span => span.classList.remove("active"))
+    characters[charIndex].classList.add("active")
+
+    let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60)
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+    mistakeTag.innerText = mistakes;
+    wpmTag.innerText = wpm;
+    cpmTag.innerText = charIndex - mistakes;
+  } else {
+    inpField.value = ""
+    clearInterval(timer)
   }
-
-  character.forEach(span => span.classList.remove("active"))
-  character[charIndex].classList.add("active")
-
-  let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60)
-  mistakeTag.innerText = mistakes
-  wpmTag.innerText = wpm
-  cpmTag.innerText = charIndex - mistakes
 }
 
 function initTimer() {
@@ -61,5 +71,18 @@ function initTimer() {
   }
 }
 
+function resetGame() {
+  randomParagraph()
+  inpField.value = ""
+  clearInterval(timer)
+  charIndex = mistakes = isTyping = 0
+  timeLeft = maxTime
+  timeTag.innerText = timeLeft
+  mistakeTag.innerText = mistakes
+  wpmTag.innerText = 0
+  cpmTag.innerText = 0
+}
+
 randomParagraph()
 inpField.addEventListener("input", initTyping)
+tryAgainBtn.addEventListener("click", resetGame)
